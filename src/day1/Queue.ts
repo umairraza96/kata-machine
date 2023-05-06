ec7fc2cfdb5675a22415c74a0f5c8bd01b85dcf3
Umair Raza <umair.raza666@gmail.com>
2023-05-07 03:20:41 +0500

    feat: `Stack` & `Queue` Datastructure

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,49 @@
+interface Node<T> {
+    value: T;
+    next?: Node<T>;
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        const head = this.head;
+        this.head = this.head.next;
+        this.length--;
+
+        if (this.length === 0) {
+            this.tail = this.head;
+        }
+        // if we are not using language without garbage collector we'll have to manually release the memory
+        // head.next =  undefined | null
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,38 @@
+interface Node<T> {
+    value: T;
+    prev?: Node<T>;
+}
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        const head = this.head;
+        this.head = node;
+        this.head.prev = head;
+    }
 
-}
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
+        const head = this.head;
+        this.head = this.head?.prev;
 
-}
+        return head?.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
